a3211747580ccf2b6c675927ceb689adaf49ceb8
sajal-19 <91428253+sajal-19@users.noreply.github.com>
2023-12-01 18:31:04 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,8 +15,7 @@ import Placepage from './pages/PlacePage'
 import BookingsPage from './pages/BookingsPage'
 import BookingPage from './pages/BookingPage'
 
-
-axios.defaults.baseURL='http://127.0.0.1:4000';
+axios.defaults.baseURL=import.meta.env.VITE_API_BASE_URL;
 //axios.defaults.baseURL='https://homily-api2.vercel.app';
 axios.defaults.withCredentials=true;
 function App() {
