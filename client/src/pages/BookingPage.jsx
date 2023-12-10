309693d3b4ea016a936907d52a5a37bfa8a4949f
sajal-19 <91428253+sajal-19@users.noreply.github.com>
2023-12-11 00:41:54 +0530

    Update BookingPage.jsx

--- client/src/pages/BookingPage.jsx
+++ client/src/pages/BookingPage.jsx
@@ -1,5 +1,6 @@
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
 import axios from "axios";
 import AddressLink from "../AddressLink";
 import PlaceGallery from "../PlaceGallery";
@@ -25,16 +26,25 @@ export default function BookingPage() {
 
   return (
     <div className="my-8">
+      <div className="flex gap-2">
       <h1 className="text-3xl">{booking.place.title}</h1>
+      <Link to={'/'}
+       className="bcolor p-2 rounded-3xl text-white">Add more
+      </Link>
+      </div>
       <AddressLink className="my-2 block">{booking.place.address}</AddressLink>
       <div className="bg-gray-200 p-6 my-6 rounded-2xl flex items-center justify-between">
-        <div>
-          <h2 className="text-2xl mb-4">Your booking information:</h2>
+        <div className="" >
+          <h2 className="text-2xl mb-3 flex gap-5 checker">Your booking information: 
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="-1 0 25 20" fill="green" class="w-9 h-9">
+         <path fill-rule="evenodd" d="M8.603 3.799A4.49 4.49 0 0112 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 013.498 1.307 4.491 4.491 0 011.307 3.497A4.49 4.49 0 0121.75 12a4.49 4.49 0 01-1.549 3.397 4.491 4.491 0 01-1.307 3.497 4.491 4.491 0 01-3.497 1.307A4.49 4.49 0 0112 21.75a4.49 4.49 0 01-3.397-1.549 4.49 4.49 0 01-3.498-1.306 4.491 4.491 0 01-1.307-3.498A4.49 4.49 0 012.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 011.307-3.497 4.49 4.49 0 013.497-1.307zm7.007 6.387a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+         </svg>
+           </h2>
           <BookingDates booking={booking} />
         </div>
         <div className="bg-primary p-6 text-white rounded-2xl">
           <div>Total price</div>
-          <div className="text-3xl">${booking.price}</div>
+          <div className="text-3xl">₹{booking.price}</div>
         </div>
       </div>
       <PlaceGallery place={booking.place} />
