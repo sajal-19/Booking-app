3a0e13ed4108463bcb7e1dccca201b858ac49822
sajal-19 <91428253+sajal-19@users.noreply.github.com>
2023-12-10 22:47:08 +0530

    Update IndexPage.jsx

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -30,7 +30,7 @@ export default function IndexPage() {
           <h2 className="font-bold">{place.address}</h2>
           <h3 className="text-sm text-gray-500">{place.title}</h3>
           <div className="mt-1">
-            <span className="font-bold">${place.price}</span> per night
+            <span className="font-bold">₹{place.price}</span> per night
           </div>
         </Link>
       );
@@ -46,7 +46,7 @@ export default function IndexPage() {
           <h2 className="font-bold">{place.address}</h2>
           <h3 className="text-sm text-gray-500">{place.title}</h3>
           <div className="mt-1">
-            <span className="font-bold">${place.price}</span> per night
+            <span className="font-bold">₹{place.price}</span> per night
           </div>
         </Link>
       );
